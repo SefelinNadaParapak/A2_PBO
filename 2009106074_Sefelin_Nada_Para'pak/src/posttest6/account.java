446a493e56c12b335ac9748e5570a3e42016b9d4
SefelinNadaParapak <sefelinnada@gmail.com>
2022-05-18 23:05:23 +0800

    posttest6

---
+++ 2009106074_Sefelin_Nada_Para'pak/src/posttest6/account.java
@@ -0,0 +1,12 @@
+package posttest6;
+
+public interface account {
+    
+    static int jumlah_suara_norut1 = 50;
+    static int jumlah_suara_norut2 = 40;
+    static int jumlah_suara_norut3 = 10;
+    
+    void masukan();
+    void ubah();
+    void hapus();
+}
---
+++ 2009106074_Sefelin_Nada_Para'pak/src/posttest6/implementasi.java
@@ -0,0 +1,16 @@
+
+package posttest6;
+
+public class implementasi {
+    public void masukkan(){
+        System.out.println("selesai memasukkan");
+    }
+    public void ubah(){
+        System.out.println("selesai mengubah");
+    }
+    public void hapus(){
+        System.out.println("selesai menghapus");
+        
+    }
+    
+}
